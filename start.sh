b4a2b123af208bfd9c2f993bbac63c1feda2c93d
Steven Riley <steveriley@Stevens-MacBook-Pro.local>
2018-01-31 15:24:49 +1100

    fix: build

--- start.sh
+++ start.sh
@@ -3,5 +3,10 @@ TF_LOG='DEBUG'
 echo ${1}
 
 go build -ldflags -w -o terraform/${1}/terraform-provider-kong
+chmod +x terraform/${1}/terraform-provider-kong
+
 cd terraform/${1}
+
+terraform init
 terraform ${2}
+
